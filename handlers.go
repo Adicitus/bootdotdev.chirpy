145855cdfb8627be8bbe2c3f0520afb598f89bd0
Joakim Olsson <joakim.olsson@adicitus.cloud>
2026-05-20 22:10:22 +0200

    Decluttered main.go

---
+++ handlers.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func handleHealthz(_ *ApiStats) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte("OK"))
+	}
+}
+
+func handleAdminMetrics(stats *ApiStats) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		msg := fmt.Sprintf(`
+<html>
+<body>
+<h1>Welcome, Chirpy Admin</h1>
+<p>Chirpy has been visited %d times!</p>
+</body>
+</html>`,
+			stats.hits.Load())
+		sr := strings.NewReader(msg)
+		msg_b := make([]byte, len(msg))
+		_, err := sr.Read(msg_b)
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		w.WriteHeader(200)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write(msg_b)
+	}
+}
+
+func handleAdminReset(stats *ApiStats) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		stats.Reset()
+	}
+}
+
+func handleValidateChirp(_ *ApiStats) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.ContentLength == 0 {
+			w.WriteHeader(400)
+			w.Write([]byte("No chirp submitted"))
+		}
+		defer r.Body.Close()
+
+		chirp := new(Chirp)
+		err := json.NewDecoder(r.Body).Decode(chirp)
+
+		if err != nil {
+			reportError(w, err)
+			return
+		}
+
+		err = validateChirp(chirp)
+
+		if err != nil {
+			reportError(w, err)
+			return
+		}
+
+		data, err := json.Marshal(ChirpValid{
+			Valid: true,
+		})
+
+		if err != nil {
+			w.WriteHeader(500)
+			w.Write([]byte("Server error"))
+			return
+		}
+
+		w.WriteHeader(200)
+		w.Write(data)
+	}
+}
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 )
 
 func reportError(w http.ResponseWriter, err error) {
@@ -25,77 +24,15 @@ func reportError(w http.ResponseWriter, err error) {
 
 func main() {
 
-	var stats ApiStats
+	stats := new(ApiStats)
 	files := http.StripPrefix("/app", http.FileServer(http.Dir("./static")))
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.Write([]byte("OK"))
-	})
-
 	mux.Handle("/app/", stats.HitsCounter(files))
-
-	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
-		msg := fmt.Sprintf(`
-<html>
-  <body>
-    <h1>Welcome, Chirpy Admin</h1>
-    <p>Chirpy has been visited %d times!</p>
-  </body>
-</html>`, stats.hits.Load())
-		sr := strings.NewReader(msg)
-		msg_b := make([]byte, len(msg))
-		_, err := sr.Read(msg_b)
-
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		w.WriteHeader(200)
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.Write(msg_b)
-	})
-	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
-		stats.Reset()
-	})
-	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
-		if r.ContentLength == 0 {
-			w.WriteHeader(400)
-			w.Write([]byte("No chirp submitted"))
-		}
-		defer r.Body.Close()
-
-		chirp := new(Chirp)
-		err := json.NewDecoder(r.Body).Decode(chirp)
-
-		if err != nil {
-			reportError(w, err)
-			return
-		}
-
-		err = validateChirp(chirp)
-
-		if err != nil {
-			reportError(w, err)
-			return
-		}
-
-		data, err := json.Marshal(ChirpValid{
-			Valid: true,
-		})
-
-		if err != nil {
-			w.WriteHeader(500)
-			w.Write([]byte("Server error"))
-			return
-		}
-
-		w.WriteHeader(200)
-		w.Write(data)
-	})
+	mux.HandleFunc("GET /api/healthz", handleHealthz(stats))
+	mux.HandleFunc("GET /admin/metrics", handleAdminMetrics(stats))
+	mux.HandleFunc("POST /admin/reset", handleAdminReset(stats))
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp(stats))
 
 	var server http.Server
 
