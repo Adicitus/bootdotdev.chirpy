6911a45b448b5b0da0577e1f1cb902db667d8a07
Joakim Olsson <joakim.olsson@adicitus.cloud>
2026-05-26 15:05:09 +0200

    Completed CH6, L6 (JWTs)

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -2,8 +2,11 @@ package auth
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/alexedwards/argon2id"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 func ValidatePassword(password string) error {
@@ -20,4 +23,29 @@ func HashPassword(password string) (hash string, err error) {
 
 func VerifyPassword(password, hash string) (correct bool, err error) {
 	return argon2id.ComparePasswordAndHash(password, hash)
+}
+
+func CreateToken(userID uuid.UUID, valididty time.Duration, secret []byte) (string, error) {
+	now := time.Now()
+	exp := now.Add(valididty)
+
+	t := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"exp": exp.Unix(),
+			"iat": now.Unix(),
+			"nbf": now.Unix(),
+			"sub": userID.String(),
+			"iss": "chirpy-access",
+		})
+
+	return t.SignedString(secret)
+}
+
+func VerifyToken(tokenString string, secret []byte) (verified bool, err error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
+		return secret, nil
+	})
+
+	return token.Valid, err
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -2,11 +2,13 @@ package auth_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Adicitus/bootdotdev.chirpy/internal/auth"
+	"github.com/google/uuid"
 )
 
-func TestHashPassword(t *testing.T) {
+func TestPasswords(t *testing.T) {
 	password := "123456"
 	t.Logf("Creating a hash of '%s'...", password)
 	hash, err := auth.HashPassword(password)
@@ -41,6 +43,59 @@ func TestHashPassword(t *testing.T) {
 
 	if verified {
 		t.Log("Verified incorrect password!")
+		t.Fail()
+	}
+}
+
+func TestTokens(t *testing.T) {
+	secret1 := []byte("Ssh! This is a secret!")
+	secret2 := []byte("Not a secret!")
+	id := uuid.New()
+
+	tokenString, err := auth.CreateToken(id, 5*time.Minute, secret1)
+
+	if err != nil {
+		t.Logf("failed to create token string: %s", err)
+		t.Fail()
+	}
+
+	verified, err := auth.VerifyToken(tokenString, secret1)
+
+	if err != nil {
+		t.Logf("Failed to verify token: %s", err)
+		t.Fail()
+	}
+
+	if !verified {
+		t.Log("Valid token not verified")
+		t.Fail()
+	}
+
+	verified, err = auth.VerifyToken(tokenString, secret2)
+
+	if err == nil {
+		t.Logf("Expected error when verifying using the wrong key")
+		t.Fail()
+	}
+
+	if verified {
+		t.Log("Verified token with the wrong secret")
+		t.Fail()
+	}
+
+	tokenString2, err := auth.CreateToken(id, 5*time.Millisecond, secret1)
+
+	time.Sleep(5 * time.Millisecond)
+
+	verified, err = auth.VerifyToken(tokenString2, secret1)
+
+	if err == nil {
+		t.Logf("Expected an error due to expired token")
+		t.Fail()
+	}
+
+	if verified {
+		t.Log("Verified an expired token")
 		t.Fail()
 	}
 }
